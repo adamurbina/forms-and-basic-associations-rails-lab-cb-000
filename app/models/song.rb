21fa8360554d94fe9eec7785c7e9a3b8682c3f72
Adam Urbina <adamurbina@gmail.com>
2018-10-09 19:18:35 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,7 +11,11 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
   end
 
 end
