79a29a75e7958804b627459be4b637b3a3601aea
Adam Urbina <adamurbina@gmail.com>
2018-10-09 19:54:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -35,7 +35,7 @@ class Song < ActiveRecord::Base
     content_string = ''
     content.each do |phrase|
       if phrase != ''
-        content_string = content_string + phrase
+        content_string = content_string + "+" + phrase
       end
     end
     content_string
